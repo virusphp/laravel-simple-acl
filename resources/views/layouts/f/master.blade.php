a7ac6529610fa7bbe343a96300c850717e05a14d
virusphp <packercyber@gmail.com>
2018-10-26 00:01:55 +0700

    update seo

--- resources/views/layouts/f/master.blade.php
+++ resources/views/layouts/f/master.blade.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="author" content="PEKALONGANINFO">
-
+  <meta name="google-site-verification" content="HpziBw6YVTEPc0xztAmxGwb0UEwjf7haS8FX4AZQtX0" />
   {{--  ==========twiter=============  --}}
   <meta name="twitter:card" content="summary_large_image" />
   <meta name="twitter:site" content="@pekalonganinfo" />
