e2154170c41635b3cf7dedfe80636bf584883058
virusphp <packercyber@gmail.com>
2018-10-27 23:30:53 +0700

    update site map

--- app/Http/Requests/CategoryDestroyRequest.php
+++ app/Http/Requests/CategoryDestroyRequest.php
@@ -14,25 +14,16 @@ class CategoryDestroyRequest extends FormRequest
     {
         return !($this->route('categories') == config('cms.default_category_id'));
     }
-<<<<<<< HEAD
-
-=======
->>>>>>> bfd43eaa597743262972d1fbd155e875147e48d3
     public function forbiddenResponse()
     {
         $notif = [
             'alert-type' => 'warning',
             'message' => 'Tidak di perbolehkan menghapus Kategori Default!!'
         ];
-<<<<<<< HEAD
 
         return redirect()->back()->with($notif);
     }
 
-=======
-        return redirect()->back()->with($notif);
-    }
->>>>>>> bfd43eaa597743262972d1fbd155e875147e48d3
     /**
      * Get the validation rules that apply to the request.
      *
@@ -44,8 +35,4 @@ class CategoryDestroyRequest extends FormRequest
             //
         ];
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> bfd43eaa597743262972d1fbd155e875147e48d3
