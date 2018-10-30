a74c694094790c6f1b8487dd94a5b935040c9753
virusphp <packercyber@gmail.com>
2018-10-30 19:58:43 +0700

    udate iklan kanan dan kirim random

--- resources/views/f/iklans/iklanwebkanan.blade.php
+++ resources/views/f/iklans/iklanwebkanan.blade.php
@@ -1,4 +1,4 @@
 
 <div class="col-2 iklan-web">
-        <img src="http://placeimg.com/300/900/tech" class="py-2 iklan-kanan" alt="" srcset="">
+        <img src="https://placeimg.com/300/900/tech" class="py-2 iklan-kanan" alt="" srcset="">
       </div>
--- resources/views/f/iklans/iklanwebkiri.blade.php
+++ resources/views/f/iklans/iklanwebkiri.blade.php
@@ -1,6 +1,6 @@
 <div class="col-2 iklan-web">
-        <img src="http://placeimg.com/300/900/tech" class="py-2 iklan-kanan" alt="" srcset="">
+        <img src="https://placeimg.com/300/900/tech" class="py-2 iklan-kanan" alt="" srcset="">
       </div>
       <div class="col-sm-12 iklan-mobile full-width">
-         <img src="http://placeimg.com/340/50/tech" alt="" srcset="">
+         <img src="https://placeimg.com/340/50/tech" alt="" srcset="">
       </div>
